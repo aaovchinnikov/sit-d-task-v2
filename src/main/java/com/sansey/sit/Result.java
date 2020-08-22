e25be7c16701f0d84b1b23c9746556ee3e5e5768
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-08-23 01:19:13 +0300

    Added argument check to Result and some tests

--- src/main/java/com/sansey/sit/Result.java
+++ src/main/java/com/sansey/sit/Result.java
@@ -1,17 +1,38 @@
 package com.sansey.sit;
 
 import java.io.PrintStream;
+import java.util.List;
 
 /**
  * Result that can be printed to provided {@link PrintStream}.
  * @author Alexander Ovchinnikov
  */
 public final class Result {
+  /** Pairs to be used as result. */
+  private final List<Pair> pairs;
+
+  /**
+   * Main constructor.
+   * @param p - {@link List} of {@link Pair}s to be used as result.
+   */
+  public Result(final List<Pair> p) {
+    if (p == null) {
+      throw new IllegalArgumentException("Null passed as argument \"p\" of "
+        + "constructor Result(final List<Pair> p). "
+        + "Argument \"p\" should be non-null.");
+    }
+    this.pairs = p;
+  }
+
+
   /**
    * Prints {@link Result} to provided {@link PrintStream}.
    * @param out - {@link PrintStream} where {@link Result} should be printed to
    */
   public void printTo(final PrintStream out) {
-    out.println("Result");
+    out.println(pairs.size());
+    for (Pair pair: pairs) {
+      pair.printTo(out);
+    }
   }
 }
--- src/test/java/com/sansey/sit/ResultTest.java
+++ src/test/java/com/sansey/sit/ResultTest.java
@@ -5,18 +5,51 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
 class ResultTest {
   @Test
-  void test() {
+  void testNull() {
+    assertThrows(
+      IllegalArgumentException.class, 
+      () -> { 
+        new Result(null); 
+      }
+    );
+  }
+  
+  @Test
+  void testPrintZeroPairs() {
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    new Result().printTo(
+    new Result(
+      Collections.emptyList()
+    ).printTo(
       new PrintStream(baos)
     );
     assertEquals(
-      "Result" + System.lineSeparator(),
+      "0" + System.lineSeparator(),
+      baos.toString(StandardCharsets.UTF_8)
+    );
+  }
+  
+  @Test
+  void testPrintTwoPairs() {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    new Result(
+      Arrays.asList(
+        new Pair(1, 2),
+        new Pair(3, 4)
+      )
+    ).printTo(
+      new PrintStream(baos)
+    );
+    assertEquals(
+      "2" + System.lineSeparator()
+        + "1 2" + System.lineSeparator()
+        + "3 4" + System.lineSeparator(),
       baos.toString(StandardCharsets.UTF_8)
     );
   }
