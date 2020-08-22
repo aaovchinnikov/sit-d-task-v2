939d69f12e9737bfea2e6727a430019799094dda
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-08-22 21:31:53 +0300

    Initial commit
    Configured usage of PMD static analyzer with basic ruleset.
    PMD runs during build.
    Configured testing with jUnit5 during build.

---
+++ src/main/java/com/sansey/sit/Result.java
@@ -0,0 +1,9 @@
+package com.sansey.sit;
+
+import java.io.PrintStream;
+
+public class Result {
+	public void printTo(PrintStream out) {
+		out.println("Result");
+	}
+}
---
+++ src/test/java/com/sansey/sit/ResultTest.java
@@ -0,0 +1,21 @@
+package com.sansey.sit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+	@Test
+	void test() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		new Result().printTo(new PrintStream(baos));
+		assertEquals(
+			"Result",// + System.lineSeparator(), 
+			baos.toString(StandardCharsets.UTF_8)
+		);
+	}
+}
